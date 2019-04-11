d7c24890007f902913e4d28a910fcd7491bd7ce4
John DongHyun Kim <zapping0320@gmail.com>
2019-04-12 08:07:23 +0900

    table view 기본 설정 테스트함

--- healthAppExtand/FirstViewController.swift
+++ healthAppExtand/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
     @IBOutlet weak var tableView: UITableView!
     @IBAction func updateWalkInfo(_ sender: UIButton) {
     }
@@ -17,7 +17,23 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        
+        return cell
+    
+    }
+    
+    
 }
+
+
 
