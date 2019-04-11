7ebcf319487188ae102c53be5df13b76f4588873
John DongHyun Kim <zapping0320@gmail.com>
2019-04-12 07:16:35 +0900

    Initial Commit

---
+++ healthAppExtand/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  healthAppExtand
+//
+//  Created by 김동현 on 12/04/2019.
+//  Copyright © 2019 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ healthAppExtand/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  healthAppExtand
+//
+//  Created by 김동현 on 12/04/2019.
+//  Copyright © 2019 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
